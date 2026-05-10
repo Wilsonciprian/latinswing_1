aafc043855ff736682f1d4253ad05d0c938d03e7
Wilsonciprian <wilson.ciprian@gmail.com>
2026-05-09 19:50:11 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // This line ensures the site loads correctly on GitHub Pages subfolders
-  base: './',
-  server: {
-    host: "0.0.0.0",
-    port: 8080,
-  },
+  // REPLACE 'latinswing' with your EXACT repo name if it's different
+  base: '/latinswing/', 
   plugins: [react()],
   resolve: {
     alias: {
