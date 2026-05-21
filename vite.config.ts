3f206d415db168808a307d32f766cf8bb00082ce
Wilsonciprian <wilson.ciprian@gmail.com>
2026-05-21 17:47:43 -0500

    Update vite.config.ts
    going back to original_1

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,23 @@
+@@ -0,0 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { componentTagger } from "lovable-tagger";
 
-export default defineConfig({
-  // REPLACE 'latinswing' with your EXACT repo name if it's different
-  base: '/latinswing/', 
-  plugins: [react()],
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080,
+    hmr: {
+      overlay: false,
+    },
+  },
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+    dedupe: ["react", "react-dom", "react/jsx-runtime", "react/jsx-dev-runtime", "@tanstack/react-query", "@tanstack/query-core"],
   },
-});
+}));
